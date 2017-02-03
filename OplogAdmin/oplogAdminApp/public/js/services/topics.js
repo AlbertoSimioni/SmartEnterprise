6694c548ce64375ec757ca6a3598c74c8f1e9720
Alberto Simioni <simioni.alberto@gmail.com>
2017-02-03 17:59:09 +0100

    small changes to api calls on admin app web interface

--- OplogAdmin/oplogAdminApp/public/js/services/topics.js
+++ OplogAdmin/oplogAdminApp/public/js/services/topics.js
@@ -5,18 +5,18 @@ angular.module('topicService', [])
 	.factory('Topics', ['$http',function($http) {
 		return {
 			get : function() {
-				return $http.get('/api/topics');
+				return $http.get('api/topics');
 			},
 			create : function(topicData) {
-				return $http.post('/api/topics', topicData);
+				return $http.post('api/topics', topicData);
 			},
 			delete : function(id) {
-				return $http.delete('/api/topics/' + id);
+				return $http.delete('api/topics/' + id);
 			},
 			update: function(id, status){
 				var data = {};
 				data.status = status;
-				return $http.put('/api/topics/' + id,data);
+				return $http.put('api/topics/' + id,data);
 			}
 		}
 	}]);
