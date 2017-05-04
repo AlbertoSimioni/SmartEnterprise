f6eaa349aa32cc6840a5c4b63eb057e55f086251
Alberto Simioni <simioni.alberto@gmail.com>
2017-05-04 15:33:31 +0200

    asd

--- SimulationMicroservices/TaskMicroservices/PuD-Availabilities/server.js
+++ SimulationMicroservices/TaskMicroservices/PuD-Availabilities/server.js
@@ -17,7 +17,7 @@ if (cluster.isMaster) {
 
 
 	app.use(express.static('./public'));    // set the static files location /public/img will be /img for users
-	//app.use(morgan('dev')); // log every request to the console
+	app.use(morgan('dev')); // log every request to the console
 	app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 	app.use(bodyParser.json()); // parse application/json
 	app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
